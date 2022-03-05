cd84b68dfed7da5e6958c3758da8b240b1e2274e
dmpr0116 <dmytro.proskurnia@netcracker.com>
2022-03-05 22:44:20 +0200

    upd

--- index.js
+++ index.js
@@ -1,57 +1,106 @@
-const {Telegraf} = require('telegraf');
+const {Telegraf, Markup} = require('telegraf');
 const {Client} = require("@notionhq/client");
-const express = require('express')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const notion = new Client({auth: process.env.NOTION_TOKEN})
-const databaseId = process.env.NOTION_DATABASE;
+const devId = 230373802;
 
-bot.command('start', ctx => ctx.reply('Привіт, як ти? Розкажи мені'))
+const USERS_TABLE_ID = 'e10d69e91bc54d208028f7c1a321f3f7';
+const MEMORIES_PARENT_PAGE_ID = 'a16a72e22c0a480b8cd54cd4d54d0386';
+
+bot.command('start', ctx => {
+    ctx.reply('Привіт, як тебе звати?', Markup.keyboard([ctx.chat.first_name, ctx.chat.username]));
+})
 bot.on('message', (ctx) => {
-    notion.pages.create({
-        parent: {
-            database_id: databaseId
-        }, properties: {
-            telegramId: {
-                title: [{
-                    text: {
-                        content: String(ctx.chat.id)
-                    }
-                }]
-            }, Message: {
-                rich_text: [{
-                    text: {
-                        content: ctx.message.text
-                    }
-                }]
+    notion.databases.query({
+        database_id: USERS_TABLE_ID, filter: {
+            property: 'telegramId', title: {
+                equals: String(ctx.chat.id)
             }
         }
-    }).then(() => ctx.reply("Дякую, записав"))
-        .catch(() => ctx.reply("Ой, щось не вийшло, спробуй пізіше"))
+    }).then(({results}) => {
+        if (!results.length) {
+            return notion.pages.create({
+                parent: {
+                    page_id: MEMORIES_PARENT_PAGE_ID,
+                }, properties: {
+                    title: [{text: {content: ctx.message.text}}]
+                }
+            }).then(({id: personalPageId}) => notion.pages.create({
+                parent: {
+                    database_id: USERS_TABLE_ID
+                }, properties: {
+                    telegramId: {
+                        title: [{text: {content: String(ctx.chat.id)}}]
+                    }, name: {
+                        rich_text: [{text: {content: ctx.message.text}}]
+                    }, personalPageId: {
+                        rich_text: [{text: {content: personalPageId}}]
+                    }
+                }
+            })).then(() => ctx.reply(`Радий знайомству, ${ctx.message.text}`, {
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            }))
+        } else {
+            if (ctx.message.text) {
+                return notion.blocks.children.append({
+                    block_id: results[0].properties.personalPageId.rich_text[0].text.content, children: [{
+                        paragraph: {rich_text: [{text: {content: ctx.message.text}}]}
+                    }]
+                })
+            } else if (ctx.message.voice) {
+                return ctx.telegram.getFileLink(ctx.message.voice.file_id).then(url => notion.blocks.children.append({
+                    block_id: results[0].properties.personalPageId.rich_text[0].text.content, children: [{
+                        audio: {
+                            external: {url},
+                            caption: ctx.message.caption && [{text: {content: ctx.message.caption}}],
+                        }
+                    }]
+                }))
+            } else if (ctx.message.photo) {
+                return ctx.telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id).then(url => notion.blocks.children.append({
+                    block_id: results[0].properties.personalPageId.rich_text[0].text.content, children: [{
+                        image: {
+                            external: {url},
+                            caption: ctx.message.caption && [{text: {content: ctx.message.caption}}],
+                        },
+                    }]
+                }))
+            } else if (ctx.message.video) {
+                return ctx.telegram.getFileLink(ctx.message.video.file_id).then(url => notion.blocks.children.append({
+                    block_id: results[0].properties.personalPageId.rich_text[0].text.content, children: [{
+                        video: {
+                            external: {url},
+                            caption: ctx.message.caption && [{text: {content: ctx.message.caption}}],
+                        }
+                    }]
+                }))
+            } else if (ctx.message.document) {
+                return ctx.telegram.getFileLink(ctx.message.document.file_id).then(url => notion.blocks.children.append({
+                    block_id: results[0].properties.personalPageId.rich_text[0].text.content, children: [{
+                        file: {
+                            external: {url},
+                            caption: ctx.message.caption && [{text: {content: ctx.message.caption}}],
+                        },
+                    }]
+                }))
+            } else {
+                ctx.reply('Вибач, я поки не розумію такі повідомлення')
+                bot.sendMessage(devId, "Повідомлення не підтримується:")
+                bot.sendMessage(devId, JSON.stringify(ctx.message))
+            }
+        }
+    });
 })
 
 bot.launch(process.env.NODE_ENV === 'production' ? {
     webhook: {
-        domain: process.env.HEROKU_URL,
-        port: Number(process.env.PORT)
+        domain: process.env.HEROKU_URL, port: Number(process.env.PORT)
     }
 } : {})
-
-const app = express()
-app.use(express.json())
-
-// app.post('/' + process.env.BOT_TOKEN, (req, resp) => {
-// app.post('/telegraf/41ba335aaa7fbb808c03006bde7add9345871f9f802a61756e9bc6ee9f976a04', (req, resp) => {
-//     console.log(req.body)
-//     bot.processUpdate(req.body)
-//     resp.sendStatus(200);
-// })
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
-
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log(`Example app listening on port ${process.env.PORT || 3000}`)
-// })
