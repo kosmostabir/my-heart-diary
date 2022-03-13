e2d61bd52ee026b6003377573d2affa44aeb22b4
dmpr0116 <dmytro.proskurnia@netcracker.com>
2022-03-14 00:15:11 +0200

    basic functionality

---
+++ server/memory.service.ts
@@ -0,0 +1,25 @@
+import {DocumentMemory, Memory, User} from "./models";
+import {PoolClient} from "pg";
+
+export class MemoryService {
+    constructor(private client: PoolClient) {
+    }
+
+    public getMemories(userId: User['userId']) {
+        return this.client.query<Memory>('SELECT * from memories where "userId" = $1 ORDER BY timestamp', [userId])
+            .then(result => result.rows)
+    }
+
+    public addMemory({
+                         userId,
+                         memoryId,
+                         text,
+                         fileId,
+                         type,
+                         timestamp
+                     }: Memory & Partial<Pick<DocumentMemory, 'fileId'>>) {
+        return this.client.query(`INSERT into memories ("userId", "memoryId", text, type, "fileId", timestamp)
+                                  VALUES ($1, $2, $3, $4, $5, $6)`,
+            [userId, memoryId, text, type, fileId, timestamp])
+    }
+}
