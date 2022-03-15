2b662577c217a0969c0bb32a33938194a21df4e5
dmpr0116 <dmytro.proskurnia@netcracker.com>
2022-03-15 16:32:31 +0200

    v2

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import {Bot} from "./bot";
 import {Pool} from "pg";
 import {MemoryService} from "./memory.service";
 
-const {Client: Notion} = require("@notionhq/client");
+// const {Client: Notion} = require("@notionhq/client");
 
 const DEV_ID = 230373802;
 const CURATORS: number[] = JSON.parse(`[${process.env.CURATORS || DEV_ID}]`);
@@ -21,7 +21,7 @@ new Pool({
     const memoriesService = new MemoryService(client);
     const bot = new Bot(userService, memoriesService);
 
-    const notion = new Notion({auth: process.env.NOTION_TOKEN})
+    // const notion = new Notion({auth: process.env.NOTION_TOKEN})
 
     const secretKey = createHash('sha256').update(bot.token).digest();
 
@@ -38,47 +38,47 @@ new Pool({
         bot.stop('SIGTERM')
     })
 
-    app.get("/api/move", (req, res) => {
-            function getPageOfUsers(cursor?: string) {
-                return notion.databases.query({
-                    database_id: 'ada738a563ac4376956f47ef5ccb2294',
-                    start_cursor: cursor,
-                }).then((page) => {
-                    return Promise.all(page.results.map(user => notion.blocks.children.list({
-                            block_id: user.properties.personalPageId.rich_text[0].text.content
-                        }).then(page => {
-                            return userService.createUser({
-                                userId: user.properties.telegramId.title[0].plain_text,
-                                name: user.properties.name.rich_text[0].plain_text.slice(0, 50),
-                                consent: user.properties.consent.checkbox
-                            }).catch((e) => {
-                                console.trace(e);
-                                console.log(user.properties.telegramId.title[0].plain_text,
-                                    user.properties.name.rich_text[0].plain_text,
-                                    user.properties.consent.checkbox
-                                )
-                            })
-                            //.then(() =>
-                            //             notion.blocks.children.list({
-                            //                 block_id: user.properties.personalPageId.rich_text[0].plain_text
-                            //             }).then(page => {
-                            //                 page.results.forEach(memory => {
-                            //                     const m: Memory = {
-                            //                         text: memory.paragraph.rich_text[1].plain_text,
-                            //                         type: MemoryType.TEXT,
-                            //                     }
-                            //                 })
-                            //             }).catch(console.log)
-                            //         })
-                            //     ));
-                        }))
-                    ).then(() => page.has_more ? getPageOfUsers(page.next_cursor) : Promise.resolve())
-                })
-            }
-
-            getPageOfUsers().then(() => console.log("DONE"))
-        }
-    )
+    // app.get("/api/move", (req, res) => {
+    //         function getPageOfUsers(cursor?: string) {
+    //             return notion.databases.query({
+    //                 database_id: 'ada738a563ac4376956f47ef5ccb2294',
+    //                 start_cursor: cursor,
+    //             }).then((page) => {
+    //                 return Promise.all(page.results.map(user => notion.blocks.children.list({
+    //                         block_id: user.properties.personalPageId.rich_text[0].text.content
+    //                     }).then(page => {
+    //                         return userService.createUser({
+    //                             userId: user.properties.telegramId.title[0].plain_text,
+    //                             name: user.properties.name.rich_text[0].plain_text.slice(0, 50),
+    //                             consent: user.properties.consent.checkbox
+    //                         }).catch((e) => {
+    //                             console.trace(e);
+    //                             console.log(user.properties.telegramId.title[0].plain_text,
+    //                                 user.properties.name.rich_text[0].plain_text,
+    //                                 user.properties.consent.checkbox
+    //                             )
+    //                         })
+    //                         //.then(() =>
+    //                         //             notion.blocks.children.list({
+    //                         //                 block_id: user.properties.personalPageId.rich_text[0].plain_text
+    //                         //             }).then(page => {
+    //                         //                 page.results.forEach(memory => {
+    //                         //                     const m: Memory = {
+    //                         //                         text: memory.paragraph.rich_text[1].plain_text,
+    //                         //                         type: MemoryType.TEXT,
+    //                         //                     }
+    //                         //                 })
+    //                         //             }).catch(console.log)
+    //                         //         })
+    //                         //     ));
+    //                     }))
+    //                 ).then(() => page.has_more ? getPageOfUsers(page.next_cursor) : Promise.resolve())
+    //             })
+    //         }
+    //
+    //         getPageOfUsers().then(() => console.log("DONE"))
+    //     }
+    // )
 
     function authenticate(authDataCookie) {
         const authData = JSON.parse(decodeURIComponent(authDataCookie.match(new RegExp('authToken=([^;]+)'))[1]));
